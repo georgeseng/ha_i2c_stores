94f642ae41a39faba353421de87e99944622a50e
Georges Engelmann <georges.engelmann@rwth-aachen.de>
2024-02-18 09:29:34 +0100

    sdfds

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 ARG BUILD_FROM
 FROM $BUILD_FROM
 RUN apk add --no-cache python3 py3-pip
-RUN apk add --no-cache py3-smbus
+RUN apk add --no-cache py3-smbus pcf8575
 COPY test.py /
 COPY run.sh /
 RUN chmod a+x /run.sh
--- test.py
+++ test.py
@@ -1,3 +1,18 @@
 import smbus
+from pcf8575 import PCF8575
 bus = smbus.SMBus(1)
-print('it worked!')
+
+i2c_port_num = 1
+pcf_address = 0x20
+
+#address=0x40
+#address=0x20
+
+pcf = PCF8575(i2c_port_num, pcf_address)
+
+#bus.read
+#bus.write_i2c_block_data(address, reg_write_dac, msg)
+#bus.write_byte(
+
+print('it worked: ')
+print(pcf.port)
